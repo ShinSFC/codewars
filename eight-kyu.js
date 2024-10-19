417ff19d9ecff25d08cd69987d95e4bc361f448e
ShinpeiH <dctechsf@gmail.com>
2024-10-18 19:23:39 -0700

    add html file

---
+++ eight-kyu.js
@@ -0,0 +1,14 @@
+
+// sum of array elements and treat stings as numbers
+
+// arr = [9, 3, '7', '3'];
+
+// function sumMix(x) {
+//     let result = 0;
+//     x.reduce((sum, num) => {
+//         return result += Number(num);
+//     }, 0);
+//     return result;
+// }
+
+// console.log(sumMix(arr));
