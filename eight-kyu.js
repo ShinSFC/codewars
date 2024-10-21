0d89aae896fd9f9c7d2526291a73bc5a45cc9f11
ShinpeiH <dctechsf@gmail.com>
2024-10-21 12:38:59 -0700

    change html src to seven-kyu and add a kata

--- eight-kyu.js
+++ eight-kyu.js
@@ -3,34 +3,34 @@
 
 // arr = [9, 3, '7', '3'];
 
-// function sumMix(x) {
-//     let result = 0;
-//     x.reduce((sum, num) => {
-//         return result += Number(num);
-//     }, 0);
-//     return result;
-// }
+function sumMix(x) {
+    let result = 0;
+    x.reduce((sum, num) => {
+        return result += Number(num);
+    }, 0);
+    return result;
+}
 
 // console.log(sumMix(arr));
 
 
 // repeat string x times
 // my solution
-// function repeatStr(n, s) {
-//     let str = '';
-//     for (let i = 0; i < n; i++) {
-//         str += s;
-//     }
-//     return str;
-// }
+function repeatStr(n, s) {
+    let str = '';
+    for (let i = 0; i < n; i++) {
+        str += s;
+    }
+    return str;
+}
 
 // console.log(repeatStr(3, 'n'));
 
 
 // book solution
-// function repeatStr (n, s) {
-//   return s.repeat(n);
-// }
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
 
 // remove all spaces in string
 // function noSpace(x) {
@@ -39,13 +39,13 @@
 
 // return a string 'num sheep...' * num
 
-// var countSheep = function (num){
-//   let str = '';
-//   for (let i = 1; i <= num; i++) {
-//      str += `${i} sheep...` 
-//   }
-//   return str;
-// }
+var countSheep = function (num){
+  let str = '';
+  for (let i = 1; i <= num; i++) {
+     str += `${i} sheep...` 
+  }
+  return str;
+}
 
 // Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
 
@@ -54,30 +54,28 @@
 // 2 -> 3 (1 + 2)
 // 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
 
-// var summation = function (num) {
-//   let result = 0;
-//   for (let i = 1; i < num + 1; i++) {       // instead of + 1, should have used <=
-//     result += i;
-//   }
-//   return result;
-// }
-
-// console.log(summation(2));
+var summation = function (num) {
+  let result = 0;
+  for (let i = 1; i < num + 1; i++) {       // instead of + 1, should have used <=
+    result += i;
+  }
+  return result;
+}
 
 //Determine winner in rock, paper, scissors
 
-// const rps = (p1, p2) => {
-//   if (p1 == p2) return 'Draw!';
-//   if (p1 == 'rock' && p2 == 'paper') return 'Player 2 won!';
-//   if (p1 == 'rock' && p2 == 'scissors') return 'Player 1 won!';
-//   if (p1 == 'paper' && p2 == 'rock') return 'Player 1 won!';
-//   if (p1 == 'paper' && p2 == 'scissors') return 'Player 2 won!';
-//   if (p1 == 'scissors' && p2 == 'rock') return 'Player 2 won!';
-//   if (p1 == 'scissors' && p2 == 'paper') return 'Player 1 won!';
-// }
+const rps = (p1, p2) => {
+  if (p1 == p2) return 'Draw!';
+  if (p1 == 'rock' && p2 == 'paper') return 'Player 2 won!';
+  if (p1 == 'rock' && p2 == 'scissors') return 'Player 1 won!';
+  if (p1 == 'paper' && p2 == 'rock') return 'Player 1 won!';
+  if (p1 == 'paper' && p2 == 'scissors') return 'Player 2 won!';
+  if (p1 == 'scissors' && p2 == 'rock') return 'Player 2 won!';
+  if (p1 == 'scissors' && p2 == 'paper') return 'Player 1 won!';
+}
 
 // find third angle of a triangle if given two angles
 
-// function otherAngle(a, b) {
-//   return 180 - a - b;
-// }
+function otherAngle(a, b) {
+  return 180 - a - b;
+}
--- seven-kyu.js
+++ seven-kyu.js
@@ -1,20 +1,30 @@
 // remove all '!' from a string
 
-function removeExclamationMarks(s) {
-  return s.replaceAll('!', '');
-}
+// function removeExclamationMarks(s) {
+//   return s.replaceAll('!', '');
+// }
 
 // calculate bmi and return string
 
-function bmi(weight, height) {
-  let bmi = weight / (height * height);
-  if (bmi <= 18.5) return 'Underweight';
-  if (bmi <= 25) return 'Normal';
-  if (bmi <= 30) return 'Overweight';
-  if (bmi > 30) return 'Obese';
-}
+// function bmi(weight, height) {
+//   let bmi = weight / (height * height);
+//   if (bmi <= 18.5) return 'Underweight';
+//   if (bmi <= 25) return 'Normal';
+//   if (bmi <= 30) return 'Overweight';
+//   if (bmi > 30) return 'Obese';
+// }
 
 // book solution
-const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
-                                    bmi <= 25 ? "Normal" :
-                                    bmi <= 30 ? "Overweight" : "Obese";
+// const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
+//                                     bmi <= 25 ? "Normal" :
+//                                     bmi <= 30 ? "Overweight" : "Obese";
+
+// Simple, given a string of words, return the length of the shortest word(s).
+// String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+  let arr = s.split(' ');
+  return arr.map(str => str.length).sort((a, b) => a - b).shift();
+}
+
+
