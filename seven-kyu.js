75b020a10f9c24911a9d170847b2fc35872ec33f
ShinpeiH <dctechsf@gmail.com>
2024-10-21 13:21:58 -0700

    add a kata

--- seven-kyu.js
+++ seven-kyu.js
@@ -29,6 +29,15 @@
 
 // Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
-function solution(str, ending){
-  return str.slice(str.length - ending.length) == ending;
+// function solution(str, ending){
+//   return str.slice(str.length - ending.length) == ending;
+// }
+
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+// Return true if you're better, else false!
+
+function betterThanAverage(classPoints, yourPoints) {
+  return (classPoints.reduce((total, score) => total += score) / classPoints.length) < yourPoints ? true : false
 }
