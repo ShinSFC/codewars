2f9dbe0ffdd894128355dae86041bcb170c6c578
ShinpeiH <dctechsf@gmail.com>
2024-10-20 20:13:15 -0700

    add a kata and book solution

--- seven-kyu.js
+++ seven-kyu.js
@@ -3,3 +3,18 @@
 function removeExclamationMarks(s) {
   return s.replaceAll('!', '');
 }
+
+// calculate bmi and return string
+
+function bmi(weight, height) {
+  let bmi = weight / (height * height);
+  if (bmi <= 18.5) return 'Underweight';
+  if (bmi <= 25) return 'Normal';
+  if (bmi <= 30) return 'Overweight';
+  if (bmi > 30) return 'Obese';
+}
+
+// book solution
+const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
+                                    bmi <= 25 ? "Normal" :
+                                    bmi <= 30 ? "Overweight" : "Obese";
