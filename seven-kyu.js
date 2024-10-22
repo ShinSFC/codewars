516a63c0cf6314b5836aba6654f1f17d69f3aa25
ShinpeiH <dctechsf@gmail.com>
2024-10-21 19:19:45 -0700

    added 2 katas

--- seven-kyu.js
+++ seven-kyu.js
@@ -71,7 +71,23 @@
 
 // Capitalize every word in string
 
-String.prototype.toJadenCase = function (String) {
-  let arr =  this.split(' ');
-  return arr.map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
-};
+// String.prototype.toJadenCase = function (String) {
+//   let arr =  this.split(' ');
+//   return arr.map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
+// };
+
+// Given a number and have to make it negative unless it is already negative.
+
+// function makeNegative(num) {
+//   return Math.sign(num) == -1 ? num : -num;
+// }
+
+// Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+function pipeFix(numbers){
+  arr = [];
+  for (let i = numbers[0]; i <= numbers[numbers.length - 1]; i++) {
+    arr.push(i)
+  }
+  return arr;
+}
