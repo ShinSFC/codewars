c28e9e77cdf778d0a50aa1caea68db358e5b8348
ShinpeiH <dctechsf@gmail.com>
2024-10-21 17:25:33 -0700

    add a kata

--- seven-kyu.js
+++ seven-kyu.js
@@ -38,6 +38,14 @@
 // You receive an array with your peers' test scores. Now calculate the average and compare your score!
 // Return true if you're better, else false!
 
-function betterThanAverage(classPoints, yourPoints) {
-  return (classPoints.reduce((total, score) => total += score) / classPoints.length) < yourPoints ? true : false
-}
+// function betterThanAverage(classPoints, yourPoints) {
+//   return (classPoints.reduce((total, score) => total += score) / classPoints.length) < yourPoints ? true : false
+// }
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+// If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+
+// function findNextSquare(sq) {
+//     let sqRt = Math.sqrt(sq); 
+//     return Number.isInteger(sqRt) ? Math.pow(sqRt + 1, 2) : -1
+// }
