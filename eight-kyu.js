a92f9ef790a0d2f3a2b69759c01830d81a8ab573
ShinpeiH <dctechsf@gmail.com>
2024-10-23 22:44:41 -0700

    add a kata

--- eight-kyu.js
+++ eight-kyu.js
@@ -79,3 +79,10 @@ const rps = (p1, p2) => {
 function otherAngle(a, b) {
   return 180 - a - b;
 }
+
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+// The returned value must be a string, and have "***" between each of its letters.
+
+function twoSort(s) {
+ return s.sort().shift().split('').join('***');
+}
--- seven-kyu.js
+++ seven-kyu.js
@@ -119,10 +119,10 @@
 
 // You get an array of numbers, return the sum of all of the positives ones.
 
-function positiveSum(arr) {
-  let sum = 0;
-  for (num of arr) {
-    if (num > 0) sum += num;
-  }
-  return sum;
-}
+// function positiveSum(arr) {
+//   let sum = 0;
+//   for (num of arr) {
+//     if (num > 0) sum += num;
+//   }
+//   return sum;
+// }
