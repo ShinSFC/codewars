6d56ec3f4bf3e857ddc3e69e2191e38026ba893b
ShinpeiH <dctechsf@gmail.com>
2024-10-24 13:10:47 -0700

    add a kata

--- seven-kyu.js
+++ seven-kyu.js
@@ -126,3 +126,9 @@
 //   }
 //   return sum;
 // }
+
+// Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+// function sortByLength (array) {
+//     return array.sort((a, b) => a.length - b.length);
+// }
