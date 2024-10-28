d8775134a243c23be083101ce2c824bca2f58eb0
ShinpeiH <dctechsf@gmail.com>
2024-10-28 11:47:05 -0700

    add a six kyu kata

--- six-kyu.js
+++ six-kyu.js
@@ -1,2 +1,6 @@
-// create a function that will return true if the walk the app gives you will take you exactly ten minutes
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
 
+function findUniq(arr) {
+  arr.sort((a, b) => a - b);
+  return arr[0] == arr[1] ? arr[arr.length - 1] : arr[0]
+}
