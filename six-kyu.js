0e9aa1f7c4eee3d37cb4594c6451b8b2b4bd5045
ShinpeiH <dctechsf@gmail.com>
2024-10-28 16:55:18 -0700

    add a kata

--- six-kyu.js
+++ six-kyu.js
@@ -4,3 +4,15 @@ function findUniq(arr) {
   arr.sort((a, b) => a - b);
   return arr[0] == arr[1] ? arr[arr.length - 1] : arr[0]
 }
+
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not.
+
+function isPangram(string){
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  let resultTrue = 0;
+  let resultFalse = 0;
+  for (let i = 0; i < 26; i++) {
+    string.toLowerCase().includes(alphabet[i]) ? resultTrue++ : resultFalse++;
+  }
+  return resultFalse === 0 ? true : false;
+}
