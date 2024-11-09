2b1255e88c079424ae529fc822c8d300031a2c05
ShinpeiH <dctechsf@gmail.com>
2024-11-08 18:40:49 -0800

    add a kata

--- seven-kyu.js
+++ seven-kyu.js
@@ -265,3 +265,17 @@ function SeriesSum(n) {
   return (Math.round(result * 100) / 100).toFixed(2).toString();
 
 }
+
+//Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+
+//The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+
+function calculateYears(principal, interest, tax, desired) {
+if ( principal == desired) return 0;
+let totalYears = 0;
+for ( let i = 1; principal <= desired; i++ ) {
+    principal += (principal * interest) - (principal * interest * tax);
+    totalYears = i;
+ }
+return totalYears;
+}
