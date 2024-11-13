775734692c88e84e554a801896c42f6583cf986e
ShinpeiH <dctechsf@gmail.com>
2024-11-13 12:58:28 -0800

    add a kata

--- seven-kyu.js
+++ seven-kyu.js
@@ -283,3 +283,8 @@ function SeriesSum(n) {
 // In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 
 const filter_list = l => l.filter(item => item === Number(item));
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+var number = array => array.map((item, idx) => `${idx + 1}: ${item}`);
